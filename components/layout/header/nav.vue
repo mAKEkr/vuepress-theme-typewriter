c2cf1ee01eda48183e9d0af7c1e098e9ca5ccfdc
Jinhu Baek <m@ake.kr>
2022-10-22 12:28:32 +0900

    Update nav.vue

--- components/layout/header/nav.vue
+++ components/layout/header/nav.vue
@@ -3,8 +3,15 @@
     <ul>
       <li v-for="(item, index) in $site.themeConfig.nav"
         :key="index">
-        <router-link :to="item.link">{{ item.text }}</router-link>
+        <a :href="item.link" v-if="item.outLink === true" :target="{ _blank: item.newWindow}">{{ item. text }}</a>
+        <router-link :to="item.link" v-else>{{ item.text }}</router-link>
       </li>
     </ul>
   </nav>
 </template>
+
+<script>
+export defualt {
+  name: 'layout-header-nav'
+}
+</script>
